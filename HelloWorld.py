b8e15f29cc4e9f5b4a6d3ae010ef5a9099ac876e
Chris V <123599620+ChrisV-8@users.noreply.github.com>
2023-01-26 20:25:55 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+#Write a program that prints a string to the console. 
+
+print ("Hello World!")
+
+"""Try printing some other strings.
+(This is a multi-line comment.)"""
+
+print("I hope you are having a great day!")
+print("Make sure to wash your hands today.")
+
+#Run the program without debugging with Ctrl + F5.
+
